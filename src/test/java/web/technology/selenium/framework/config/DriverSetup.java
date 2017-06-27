e503eaabcd1b5b6f2331a623a6fef23e26a982fb
kalisb <kalisb@abv.bg>
2017-06-27 06:08:25 +0300

    Initials structure.

---
+++ src/test/java/web/technology/selenium/framework/config/DriverSetup.java
@@ -0,0 +1,16 @@
+package web.technology.selenium.framework.config;
+
+import org.openqa.selenium.Proxy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+/**
+ * Created by kalisb on 21.05.17.
+ */
+public interface DriverSetup {
+
+    WebDriver getWebDriverObject(DesiredCapabilities desiredCapabilities);
+
+    DesiredCapabilities getDesiredCapabilities(Proxy proxySettings);
+
+}
---
+++ src/test/java/web/technology/selenium/framework/core/view/Page.java
@@ -0,0 +1,13 @@
+package web.technology.selenium.framework.core.view;
+
+/**
+ * Created by kalisb on 28.05.17.
+ */
+public class Page implements View {
+
+    private String url;
+
+    public String url() {
+        return url;
+    }
+}
